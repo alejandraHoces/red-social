ad025c2393afe8de1befd64361776cd0e1f54e8d
Alejandra <hocesalejandra@gmail.com>
2018-01-11 15:39:27 -0500

    Añadiendo funcionalidad para subir fotos

--- js/inicio.js
+++ js/inicio.js
@@ -17,9 +17,14 @@ $(document).ready(function () {
   });
 
   $btnImage.on('click', function() {
-    var $path = $file.val();
+    var photo= $('#urlInput').val();
+    /** Vaciando los input. */
+    $('#urlInput').val('');
+    /** Contenedor donde irán los nuevos comentarios. */
+    var contenedor= $('#board');
+    /*var $path = $file.val();
     var $image = $path.substring($path.lastIndexOf('\\') + 1, $path.length);
-    console.log($image);
-    $($board).prepend('<form class="col s12"><div class="row"><div class="col s2"><img src="../assets/images/user.jpg" class="circle responsive-img"></div><div class="col s10"><p><img class="imageUpload" src="../assets/images/' + $image + '"></p></div></div></form>');
+    console.log($image);*/
+    contenedor.append('<div class=box><div> <img class="photobox center-block" src= ' + photo + '  alt=...> </div></div>');
   });
 });
